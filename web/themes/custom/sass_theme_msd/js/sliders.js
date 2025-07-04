5fd053b06f09f7f7d5c300865a5e1a6644e123d5
abdur-rahman <arafridi1232@gmail.com>
2025-07-04 16:40:37 +0500

    design new fixes with carosel created and jahagir css fixes by Abdur rehman

--- web/themes/custom/sass_theme_msd/js/sliders.js
+++ web/themes/custom/sass_theme_msd/js/sliders.js
@@ -1,6 +1,7 @@
 (function ($, Drupal) {
 	Drupal.behaviors.backgroundSlider = {
 	  attach: function (context, settings) {
+		// RTL sliders
 		$('[dir="rtl"] .background-slider', context).each(function () {
 		  if (!$(this).hasClass('slick-initialized')) {
 			$(this).slick({
@@ -9,27 +10,74 @@
 			  arrows: true,
 			  dots: true,
 			  fade: true,
-			  rtl: true,
+			  rtl: true
 			});
 		  }
 		});
-
+  
+		// LTR background sliders
 		$('[dir="ltr"] .background-slider', context).each(function () {
+		  if (!$(this).hasClass('slick-initialized')) {
+			$(this).slick({
+			  autoplay: true,
+			  autoplaySpeed: 4000,
+			  arrows: true,
+			  dots: true,
+			  fade: true,
+			  rtl: false
+			});
+		  }
+		});
+  
+		// LTR make-carosel sliders
+		$('[dir="rtl"] .make-carosel', context).each(function () {
+		  if (!$(this).hasClass('slick-initialized')) {
+			$(this).slick({
+				slidesToShow: 3,
+				slidesToScroll: 1,
+				infinite: true,
+				autoplay: true,
+				autoplaySpeed: 3000,
+				speed: 600,
+				dots: false,
+				arrows: false,
+				rtl: true,
+				responsive: [
+				  {
+					breakpoint: 768,
+					settings: {
+					  slidesToShow: 1
+					}
+				  }
+				]
+		
+			});
+		  }
+		});
+		$('[dir="ltr"] .make-carosel', context).each(function () {
 			if (!$(this).hasClass('slick-initialized')) {
 			  $(this).slick({
-				autoplay: true,
-				autoplaySpeed: 4000,
-				arrows: true,
-				dots: true,
-				fade: true,
-				ltr: true,
+				  slidesToShow: 3,
+				  slidesToScroll: 1,
+				  infinite: true,
+				  autoplay: true,
+				  autoplaySpeed: 3000,
+				  speed: 600,
+				  dots: false,
+				  arrows: false,
+				  ltr: true,
+				  responsive: [
+					{
+					  breakpoint: 768,
+					  settings: {
+						slidesToShow: 1
+					  }
+					}
+				  ]
+		  
 			  });
 			}
-		  });		
-		}
+		  });
+	  }
 	};
   })(jQuery, Drupal);
-  
-
-
-
