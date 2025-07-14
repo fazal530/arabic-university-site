ac8df940ac1e6420cebfc606bbd2df11c59359f7
abdur-rahman <arafridi1232@gmail.com>
2025-07-14 17:50:36 +0500

    i fix the slick slider

--- web/themes/custom/sass_theme_msd/js/sliders.js
+++ web/themes/custom/sass_theme_msd/js/sliders.js
@@ -1,95 +1,95 @@
 (function ($, Drupal) {
 	Drupal.behaviors.backgroundSlider = {
-	  attach: function (context, settings) {
-		// RTL sliders
-		$('[dir="rtl"] .background-slider', context).each(function () {
-		  if (!$(this).hasClass('slick-initialized')) {
-			$(this).slick({
-			  autoplay: true,
-			  autoplaySpeed: 4000,
-			  arrows: true,
-			  dots: true,
-			  fade: true,
-			  rtl: true
+		attach: function (context, settings) {
+			// RTL sliders
+			$('[dir="rtl"] .background-slider', context).each(function () {
+				if (!$(this).hasClass('slick-initialized')) {
+					$(this).slick({
+						autoplay: true,
+						autoplaySpeed: 4000,
+						arrows: true,
+						dots: true,
+						fade: true,
+						rtl: true
+					});
+				}
 			});
-		  }
-		});
-  
-		// LTR background sliders
-		$('[dir="ltr"] .background-slider', context).each(function () {
-		  if (!$(this).hasClass('slick-initialized')) {
-			$(this).slick({
-			  autoplay: true,
-			  autoplaySpeed: 4000,
-			  arrows: true,
-			  dots: true,
-			  fade: true,
-			  rtl: false
+
+			// LTR background sliders
+			$('[dir="ltr"] .background-slider', context).each(function () {
+				if (!$(this).hasClass('slick-initialized')) {
+					$(this).slick({
+						autoplay: true,
+						autoplaySpeed: 4000,
+						arrows: true,
+						dots: true,
+						fade: true,
+						rtl: false
+					});
+				}
 			});
-		  }
-		});
-  
-		// LTR make-carosel sliders
-		$('[dir="rtl"] .make-carosel', context).each(function () {
-		  if (!$(this).hasClass('slick-initialized')) {
-			$(this).slick({
-				slidesToShow: 3,
-				slidesToScroll: 1,
-				infinite: true,
-				autoplay: true,
-				autoplaySpeed: 1000,
-				speed: 600,
-				dots: false,
-				arrows: false,
-				rtl: true,
-				responsive: [
-					{
-					  breakpoint: 1024,   // below 1024px (tablet)
-					  settings: {
-						slidesToShow: 2   // show 2 slides on tablet
-					  }
-					},
-					{
-					  breakpoint: 768,    // below 768px (mobile)
-					  settings: {
-						slidesToShow: 1   // show 1 slide on mobile
-					  }
-					}
-				  ]
-		
+
+			// LTR make-carosel sliders
+			$('[dir="rtl"] .make-carosel', context).each(function () {
+				if (!$(this).hasClass('slick-initialized')) {
+					$(this).slick({
+						slidesToShow: 3,       // 👈 Number of slides (cards) shown at once
+						slidesToScroll: 1,     // 👈 Number of slides to move when scrolling/clicking arrow
+						arrows: true,          // 👈 Show previous/next arrows
+						infinite: false,       // 👈 Loop back to first slide when reaching the end (false = no loop)
+						autoplay: false,       // 👈 Automatically scroll through slides
+						autoplaySpeed: 3000,   // 👈 Time between auto scrolls (ms), only works if autoplay is true
+						speed: 500,            // 👈 Transition speed (ms) between slides
+					  
+						responsive: [          // 👈 Media query breakpoints for responsiveness
+						  {
+							breakpoint: 1024,  // 👈 If screen is 1024px or less
+							settings: {
+							  slidesToShow: 2  // 👈 Show 2 slides
+							}
+						  },
+						  {
+							breakpoint: 780,   // 👈 If screen is 768px or less
+							settings: {
+							  slidesToShow: 1, // 👈 Show 1 slide
+							  arrows: false    // 👈 Hide arrows on small screens
+							}
+						  }
+						]
+
+					});
+				}
 			});
-		  }
-		});
-		$('[dir="ltr"] .make-carosel', context).each(function () {
-			if (!$(this).hasClass('slick-initialized')) {
-			  $(this).slick({
-				  slidesToShow: 3,
-				  slidesToScroll: 1,
-				  infinite: true,
-				  autoplay: true,
-				  autoplaySpeed: 1000,
-				  speed: 600,
-				  dots: false,
-				  arrows: false,
-				  ltr: true,
-				  responsive: [
-					{
-					  breakpoint: 1024,   // below 1024px (tablet)
-					  settings: {
-						slidesToShow: 2   // show 2 slides on tablet
-					  }
-					},
-					{
-					  breakpoint: 768,    // below 768px (mobile)
-					  settings: {
-						slidesToShow: 1   // show 1 slide on mobile
-					  }
-					}
-				  ]
-		  
-			  });
-			}
-		  });
-	  }
+			$('[dir="ltr"] .make-carosel', context).each(function () {
+				if (!$(this).hasClass('slick-initialized')) {
+					$(this).slick({
+						slidesToShow: 3,       // 👈 Number of slides (cards) shown at once
+						slidesToScroll: 1,     // 👈 Number of slides to move when scrolling/clicking arrow
+						arrows: true,          // 👈 Show previous/next arrows
+						infinite: true,       // 👈 Loop back to first slide when reaching the end (false = no loop)
+						autoplay: false,       // 👈 Automatically scroll through slides
+						autoplaySpeed: 3000,   // 👈 Time between auto scrolls (ms), only works if autoplay is true
+						speed: 500,            // 👈 Transition speed (ms) between slides
+
+						responsive: [          // 👈 Media query breakpoints for responsiveness
+							{
+								breakpoint: 1440,  // 👈 If screen is 1024px or less
+								settings: {
+									slidesToShow: 2  // 👈 Show 2 slides
+								}
+							},
+							{
+								breakpoint: 780,   // 👈 If screen is 768px or less
+								settings: {
+									slidesToShow: 1, // 👈 Show 1 slide
+									arrows: false    // 👈 Hide arrows on small screens
+								}
+							}
+						]
+
+					});
+				}
+			});
+		}
 	};
-  })(jQuery, Drupal);
+})(jQuery, Drupal);
